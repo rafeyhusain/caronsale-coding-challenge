8df331bef3d7f994c00d65c819a8f041e0890826
Syed Rafey Husain <rafey.husain@gmail.com>
2020-03-19 14:58:04 +0500

    CarOnSaleClient class added with method stub

---
+++ challenges/backend/src/app/services/CarOnSaleClient/classes/CarOnSaleClient.ts
@@ -0,0 +1,9 @@
+import { ICarOnSaleClient } from '../interface/ICarOnSaleClient'
+
+export class CarOnSaleClient implements ICarOnSaleClient {
+
+    getRunningAuctions(): Promise<any /* TODO: Introduce a type */> {
+        return null;
+    }
+
+}
